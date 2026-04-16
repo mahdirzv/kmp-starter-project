190a4be555d9d822ac850e607374d000897604d4
mahdirzv <rzv.mahdi@gmail.com>
2026-04-16 09:23:02 +0100

    Flatten imported KMP pack trees

--- kmp/auth/shared/src/commonMain/kotlin/de/sh3n/kmp_starter_project/auth/di/AuthModule.kt
+++
@@ -1,9 +0,0 @@
-package de.sh3n.kmp_starter_project.auth.di
-
-import de.sh3n.kmp_starter_project.auth.data.NoOpAuthRepository
-import de.sh3n.kmp_starter_project.auth.domain.AuthRepository
-import org.koin.dsl.module
-
-val authModule = module {
-    single<AuthRepository> { NoOpAuthRepository() }
-}
--- kmp/room_data/shared/src/commonMain/kotlin/de/sh3n/kmp_starter_project/tasks/domain/Task.kt
+++
@@ -1,7 +0,0 @@
-package de.sh3n.kmp_starter_project.tasks.domain
-
-data class Task(
-    val id: String,
-    val title: String,
-    val completed: Boolean = false,
-)
